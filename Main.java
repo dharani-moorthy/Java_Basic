9221fb3b0d1fc6a525788530a4f1cbce2fee005f
Dharani M <dharanimoorthy51@gmail.com>
2025-12-27 12:34:34 +0530

    Type Conversion

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        //type convertion
+        byte a=12;
+        int b=233;
+        byte c=(byte) b;
+        System.out.println(c);
+
+    }
+}
