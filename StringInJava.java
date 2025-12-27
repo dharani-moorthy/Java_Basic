beb2ee958eb73f27706a1ec7f094f2862bcd0a0d
Dharani M <dharanimoorthy51@gmail.com>
2025-12-27 12:57:34 +0530

    String

---
+++ StringInJava.java
@@ -0,0 +1,10 @@
+public class StringInJava {
+    public static void main(String[] args) {
+        String a=new String("dhara");
+        System.out.println("hello"+a);
+        System.out.println(a.hashCode());
+        System.out.println(a.charAt(3));
+        System.out.println(a.concat("dora"));
+
+    }
+}
