1825c40ef3d97da932c6df28af22155c6ea790d5
Dharani M <dharanimoorthy51@gmail.com>
2025-12-27 12:58:29 +0530

    Mutable Vs Immutable In String

---
+++ MutableVsImmutable_String.java
@@ -0,0 +1,8 @@
+public class MutableVsImmutable_String {
+    public static void main(String[] args) {
+        // mutable ----> change
+        // immutable -----> un change
+           // * StringBuffer
+           // * Stringbuilder
+    }
+}
