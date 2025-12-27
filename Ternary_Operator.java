f2db7a54aec9bef75ff85f317c63d900789b9955
Dharani M <dharanimoorthy51@gmail.com>
2025-12-27 12:38:58 +0530

    Ternary Operator

---
+++ Ternary_Operator.java
@@ -0,0 +1,12 @@
+package Dharani;
+
+public class Ternary_Operator {
+    public static void main(String[] args) {
+        int number = 10;
+        // Using ternary operator to check even/odd
+        String result = (number % 2 == 0) ? "Even" : "Odd";
+
+        System.out.println(number + " is " + result);
+    }
+}
+
