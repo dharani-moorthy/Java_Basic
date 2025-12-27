5378719496b5572c043441c0f6b5161ccfc3b550
Dharani M <dharanimoorthy51@gmail.com>
2025-12-27 12:35:30 +0530

    Conditional Statement

---
+++ Condtional_statement.java
@@ -0,0 +1,14 @@
+public class Condtional_statement {
+    public static void main(String[] args) {
+        int number = -5;
+
+        if (number > 0) {
+            System.out.println("The number is positive.");
+        } else if (number < 0) {
+            System.out.println("The number is negative.");
+        } else {
+            System.out.println("The number is zero.");
+        }
+
+    }
+}
