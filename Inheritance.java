44695f56821510b197be18905686c6915935ed97
Dharani M <dharanimoorthy51@gmail.com>
2025-12-29 18:58:38 +0530

    Inheritance

---
+++ Inheritance.java
@@ -0,0 +1,22 @@
+class Inheritance {
+    public static void main(String[] args) {
+      //  calculator cal = new calculator();
+//        int res = cal.add(5, 9);
+//        System.out.println(res);
+//
+//        int re = cal.sub(9, 3);
+//        System.out.println(re);
+        MultiInheritance aa=new MultiInheritance();
+        int res = aa.add(5, 9);
+        System.out.println(res);
+        int re = aa.sub(9, 3);
+        System.out.println(re);
+        int r=aa.divii(40,2);
+        System.out.println(r);
+        int a=aa.power(2,6);
+        System.out.println(a);
+
+    }
+}
+
+
