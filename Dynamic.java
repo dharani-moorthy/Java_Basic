0de30e740d5e177b251a5e35d203bdf422ca602c
Dharani M <dharanimoorthy51@gmail.com>
2025-12-30 19:47:50 +0530

    Dynamic Method Dispatch In Polymorphism

---
+++ Dynamic.java
@@ -0,0 +1,26 @@
+package New;
+class A{
+    public void show(){
+        System.out.println("in A");
+    }
+}
+class B extends A {
+    public void show() {
+        System.out.println("in B");
+    }
+}
+class C extends A{
+    public void show(){
+        System.out.println("in C");
+    }
+}
+public class Dynamic {
+    public static void main(String[] args) {
+        A obj=new A();
+        obj.show();
+        obj=new B();
+        obj.show();
+        obj=new C();
+        obj.show();
+    }
+}
