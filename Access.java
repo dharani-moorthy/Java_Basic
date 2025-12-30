9695ebd04f29affe929203ed2298832a12624418
Dharani M <dharanimoorthy51@gmail.com>
2025-12-30 19:45:41 +0530

    Access Modifiers

---
+++ Access.java
@@ -0,0 +1,9 @@
+package Pack;
+
+import New.Modifier;
+public class Access {
+    public static void main(String[] args) {
+        Modifier s=new Modifier();
+        System.out.println(s.a);
+    }
+}
