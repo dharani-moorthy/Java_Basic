4043e8a93ba5e2d8298e8eff4ae800ee53b8fdf3
Dharani M <dharanimoorthy51@gmail.com>
2025-12-30 20:09:12 +0530

    Var

---
+++ Var.java
@@ -0,0 +1,16 @@
+package Dharani;
+
+public class Var {
+    public static void main(String[] args) {
+        var i= 333;
+        var j="dharani";
+        var k='D';
+        var l=23.44;  // with out assigning we cannot use var eg: var d;
+        System.out.println(i);
+        System.out.println(j);
+        System.out.println(k);
+        System.out.println(l);
+
+
+    }
+}
