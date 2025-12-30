c61f8ac5f66b565c3cd8f3f30b744c09af130f46
Dharani M <dharanimoorthy51@gmail.com>
2025-12-30 19:52:58 +0530

    Inner Class

---
+++ InnerClass.java
@@ -0,0 +1,24 @@
+package Dharani;
+
+class A{
+    int age;
+    public void show(){
+        System.out.println("in show");
+    }
+    static class B{
+        public void config(){
+            System.out.println("in config");
+        }
+
+    }
+}
+public class InnerClass {
+    public static void main(String[] args) {
+        A obj=new A();
+        obj.show();
+      // A. B obj1=  obj.new B();
+        A.B obj1 =new A.B();
+       obj1.config();
+
+    }
+}
